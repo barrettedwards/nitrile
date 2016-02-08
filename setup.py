f612746d2fe67b074b45f48b46406ae360399f07
barrett edwards <barrettedwardsoss@gmail.com>
2016-02-07 21:02:39 -0700

    adding setup.py module file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='nitrile',
+      version='0.1.0',
+      description='Latex Document Creation Library',
+      url='https://github.com/barrettedwards/nitrile',
+      author='Barrett Edwards',
+      author_email='BarrettEdwardsOSS@gmail.com',
+      license='MIT',
+      packages=['nitrile'],
+      zip_safe=False)
